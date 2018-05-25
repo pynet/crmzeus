90aa127fd2e0ab6b8eb2c3f860b026866d23da0b
Vicente Marçal <vicente.marcal@gmail.com>
2018-05-25 15:05:52 -0300

    Created Tests and Implemented the HTML to Promoter Summary
    Close #31

--- zeus/core/views.py
+++ zeus/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/index.html')
+
+
+def report(request):
+    return render(request, 'core/report.html')
--- zeus/urls.py
+++ zeus/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from zeus.core.views import home
+from zeus.core.views import home, report
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('report/', report, name='reports'),
     path('admin/', admin.site.urls),
 ]
