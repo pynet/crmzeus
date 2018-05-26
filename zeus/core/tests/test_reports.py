b2b774d32646cbe8dec2feabaff3e12d754daa87
Vicente Marçal <vicente.marcal@gmail.com>
2018-05-26 19:16:45 -0300

    Finished the promoter summary table to show to costumer for analyzing
    Close #44

--- zeus/core/tests/test_reports.py
+++ zeus/core/tests/test_reports.py
@@ -14,3 +14,17 @@ def test_report_status_code(report_resp):
 
 def test_report_template_used(report_resp):
     dj_assert_template_used(report_resp, template_name='core/report.html')
+
+
+# @pytest.mark.parametrize(
+#
+""" We need to think abou how to make tests to this template! """
+#
+#     'table',
+#     [
+#         '<table class="table">',
+#         '<th'
+#     ]
+# )
+# def test_report_contains_table(report_resp):
+#     pass
